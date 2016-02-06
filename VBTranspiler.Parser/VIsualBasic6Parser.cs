b56f02b5b841c4c599a67ff68455d02c41a7dd8e
Lee Millward <none@none>
2016-02-06 22:41:04 +0000

    Add some listener classes for debugging parser errors. When parsing code ensure that no syntax errors occurred.

--- VBTranspiler.Parser/VIsualBasic6Parser.cs
+++ VBTranspiler.Parser/VIsualBasic6Parser.cs
@@ -1,6 +1,7 @@
 ﻿#region Imports
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 #endregion
 
@@ -29,7 +31,58 @@ namespace VBTranspiler.Parser
       ITokenStream tokens = new CommonTokenStream(new VisualBasic6Lexer(input));
 
       VisualBasic6Parser parser = new VisualBasic6Parser(tokens);
-      return parser.module();
+      parser.AddParseListener(new ParserListener(parser));
+      parser.AddErrorListener(new DebugErrorListener<IToken>());
+
+      var ret = parser.module();
+
+      if (parser.NumberOfSyntaxErrors > 0)
+        throw new ApplicationException("Parser errors encountered");
+
+      return ret;
+    }
+  }
+
+  public class ParserListener : IParseTreeListener
+  {
+    private int mIndent;
+    private VisualBasic6Parser mParser;
+
+    public ParserListener(VisualBasic6Parser parser)
+    {
+      mParser = parser;
+    }
+
+    public void EnterEveryRule(ParserRuleContext ctx)
+    {
+      mIndent += 1;
+
+      Debug.Write("".PadLeft(mIndent));
+      Debug.WriteLine(string.Format("Enter {0}", mParser.RuleNames[ctx.RuleIndex]));
+    }
+
+    public void ExitEveryRule(ParserRuleContext ctx)
+    {
+      Debug.Write("".PadLeft(mIndent));
+      Debug.WriteLine(string.Format("Exit {0}", mParser.RuleNames[ctx.RuleIndex])); 
+      
+      mIndent -= 1;
+    }
+
+    public void VisitErrorNode(IErrorNode node)
+    {
+    }
+
+    public void VisitTerminal(ITerminalNode node)
+    {
+    }
+  }
+
+  public class DebugErrorListener<Symbol> : IAntlrErrorListener<Symbol>
+  {
+    public virtual void SyntaxError(IRecognizer recognizer, Symbol offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+    {
+      Debug.WriteLine("line " + line + ":" + charPositionInLine + " " + msg);
     }
   }
 }
